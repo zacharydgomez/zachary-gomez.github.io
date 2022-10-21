6be1d875bf7f8ceb178d2e00e0cd4e3089ff07ad
zacharydgomez <zacharydgomez@gmail.com>
2022-10-21 17:19:27 +0000

    studies variables

--- studies/variables.js
+++ studies/variables.js
@@ -6,31 +6,30 @@
 | | | | / _` || '__|| | / _` || '_ \ | | / _ \/ __|
 \ \_/ /| (_| || |   | || (_| || |_) || ||  __/\__ \
  \___/  \__,_||_|   |_| \__,_||_.__/ |_| \___||___/
-                                                   
-                                                   
-
+                                                                                                   
  * 
  * Summary: Variables are containers for storing data in Javascript.
  *  Variables can contain the values of primitives( such as values like Number, String, Boolean),
  * or in the case of a complex value( Object, Array, and function) variables point to a loction in memory where the value is stored.
- * Variables can then be used to be called up in the code are various places.
+ * Variables can then be used to be called up in the code in various places.
  * 
- * Declaring a variable
+ * //Declaring a variable//
  * 
  *  In Javascript, creating a variable is called "declaring".
  * Until you intitize a variable the value is undefined
  * 
- * Example!!
+ * !!Example!!
  * var bigDog;
  * console.log(bigDog) => undefined
  * let grouchyCat;
  * console.log(grouchyCat) => undefined
  * 
  * 
- * Initializing a variable
+ * // Initializing a variable//
  * 
  * Variables must be declared in JavaScript with var, let, or const
- *   Example!!
+ * 
+ *   !!Example!!
  * let x = "dog"
  * console.log(x) => prints dog
  * var y = true  
@@ -44,7 +43,8 @@
   * The let keyword should be used any time  the value of your variable could change
   * The const keyword should be used any time you want the value of the variable to be fixed
   * The var keyword is from the pre 2015 edition of Javascript and should be used when the script must run in an older web browser.
-  * Example!!
+  * 
+  * !!Example!!
   * const amount1 = 20
   * const amount2 = 5
   * let total = amount1 + amount2
@@ -55,16 +55,17 @@
  * total is set with the let keyword because it is a value that could be changed by a future part of the code.
  * 
  * 
- * Re-assignment
+ * //Re-assignment//
  *  In the case of variables assigned with var or let they may be reassigned.
- * Example!!
+ * 
+ * !!Example!!
  * x = "cat"
  * console.log(x) => prints cat
  * y = false
  * console.log(y) => prints false
  * 
  * 
- * Scope
+ * //Scope//
  *  Scope is the current context that determines the visibility of variables.
  *  Before 2015 Javascript only had Global Scope and Function Scope.
  * 
@@ -78,29 +79,41 @@
  * The const and let keyword can also be included in the block scope of a function. 
  * This means that when these keywords are declared within {} they are only avaible to that block.
  * 
- * Hosting
+ * //Hosting//
  *  Hoisting is the default behavior in Javascript to move declarations to the top of the current scope.
+ * var declations are hoisted to the top but are not initilized.
  * 
- * let and const are hoisted to the top of the block but remain not initilized.
+ * !!Example!!
+ * console.log(bigDog) => prints undefined
+ * var bigDog = "koji"
+ * console.log(bigDog) => prints "koji"
+ * let and const are not hoisted to the top of the block. 
  * 
+ * !!Example!!
+ * console.log(grouchyCat) => throws a reference error
+ * console.log(y) => also throws a reference error
+ * const grouchyCat = "sart"
+ * let y = true
  * 
+ *  // var, let, const//
+ *  A further explination of the three keyword types
+ *  Each keyword has their own unique properties
+ *  use let over var in most cases 
  * 
+ *   /var/
+ *  Only keyword pre es6
+ * Allows for reassigable values
+ *  var scope includes global and function
+ * the declarations will be hoisted to the top but not the initilization
  * 
- * var
- * global scoped
- * hoisted 
- * reassignable
+ *   /let/
+ *  Considered an improvement over var
+ * Introduced in es6, let allows for reassigable values.
+ * let are block scoped and not hoisted
  * 
- * let
- * block scoped
- * not hoisted
- * reassignable
- * 
- * const
- * block scoped
- * not hoisted
- * not reassignable
- * 
- * 
+ *   /const/
+ *  Introduced in es6, const allows variables values to become immmutable.
+ *  const are block scoped and not hoisted
+ *  Must be initilized when declared
  * 
  */
