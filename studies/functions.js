4c513da6c0524b415aaafd9dbd7a37fc826294d3
zacharydgomez <zacharydgomez@gmail.com>
2022-10-27 23:06:47 +0000

    functions

--- studies/functions.js
+++ studies/functions.js
@@ -11,13 +11,40 @@
 
  */
 /**
- * Summary: Functions are blocks of reusable code that do something
+ * Summary: Functions are blocks of reusable code that can perform the same action in many places.
+ * They are an essential for programing in Javascript.
+ * Functions take in some input and return an output.
+ * Javascript has built in functions but it is also possible to create new functions.
  * 
  * Defining Functions
- *  
- * !!Example
+ *  Functions can be declared as function expressions, function statements, and as arrow functions.
+ * Function declaration starts with the keyword function, followed by the name of the function,
+ *  a list of parameters enclosed in parenthese and seperated by commas.
+ * 
+ * !!Example!!
+ * function makeBig(size, increase){
+ *  return size + increase;}
+ * 
+ *  Function expressions are created by declaring a variable and initializing it to a function. 
+ * It is possible to have an anonymous function defined this way.
+ * 
+ *  !!Example!!
+ *  const small = function makeSmall(size, decrease){
+ *  return size - decrease}
+ * 
+ * Or in the case of an anonymouse function
+ *  const double - function( size, exponent){
+ *  return size ** exponent }
+ * 
+ *  It is also possible to declare a function within a object. These are called methods. 
+ * 
  * Function Parameters
+ *  Parameters are the names listed in the function definition that arguments are passed into.
+ * Parameters are just names while arguments are the actual values.
+ * 
  * Calling Functions 
+ *  Once a function has been defined to actually perform the actions indiciated in the function it must be called.
+ * 
  * Function Scope 
  * Arrow functions
  * Anonymous Functions
