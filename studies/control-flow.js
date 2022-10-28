34971e952fac3432e0c72425d329e78a431947c0
zacharydgomez <zacharydgomez@gmail.com>
2022-10-28 19:26:16 +0000

    control flow

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -23,7 +23,8 @@
    * 
    * //Conditional Statements//
    * Conditional statements are used when you want different actions perform depending on the different conditions.
-   * In javascript there are four conditonal statements, if, else if, else, switch. 
+   * In javascript there are  conditonal statements such as if, else if, else, switch and ternary operators. 
+   * They effect control flow by creating branching paths and directed the logic to go a certain way.
    * 
    * //if//
    * The if statement is used when code is meant to be run when the condition is true.
@@ -69,7 +70,39 @@
    * //switch//
    *  The switch statement is used to evaluate an expression against a series of case clasues.
    *  It executes the first  case clause with a matching value until it encouters a break statement.
-   * The statement contains a default clause for when none of the case clauses match
+   * The statement contains a default clause for when none of the case clauses match.
+   *  Switch statements are always looking for if a condition is strictly equaly to the condition of the statement.
+   *  It is less flexable than the other kinds of conditional statements
    * !!Example!!
    * 
+   * const console = playstation
+   *  switch(console){
+   * case "xbox":
+   * console.log("its an xbox")
+   * break;
+   * case "nintendo":
+   * console.log("its a nintento")
+   * break;
+   * case "playstation":
+   * console.log("its a playstation")
+   * break;
+   * default:
+   * console.log("whats that")
+   * } // this would resolve to logging "its a playstation"
+   * 
+   * 
+   *  //Ternary Operators//
+   *  The Ternary operator also is a conditional statement.
+   * It takes in three operands. A condition to test then a ? followed by an expression if the condition is true 
+   * then a colon followed by an expression to execute if condition is false.
+   * It is possible to rewrite if.. else statements with ternary operators.
+   *  !!Example!!
+   * let age = 50;
+   * let output;
+   *  if( age >= 50){ output = "user is older than 40"}
+   * else{ output = "user is younger than 40"}
+   * 
+   *  // to rewrite with a ternary operator
+   * let age = 50
+   *  let output = age >= 40 ? "user is older than 40" : "user is younger than 40" // output would resolve to the truthy condition
    */
